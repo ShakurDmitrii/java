416becae3f95f6bafe7ffddba11dfdfd3cfb01b7
Shakur <dimapak2022004@gmail.com>
2024-09-21 12:05:00 +0300

    add Before

--- Test/src/main/java/org/example/Main.java
+++ Test/src/main/java/org/example/Main.java
@@ -1,9 +1,9 @@
 package org.example;
 public class Main {
     public static void main(String[] args) throws Exception {
-        int c = org.example.Sum.sum(2, 2);
+        int c = org.example.Count.sum(2, 2);
         System.out.println(c);
-        int d = org.example.Sum.div(1, 1);
+        int d = org.example.Count.div(1, 1);
         System.out.println(d);
     }
 }
--- Test/src/test/java/Tests.java
+++ Test/src/test/java/Tests.java
@@ -1,51 +1,71 @@
-import org.example.Sum;
+import org.example.Count;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import static org.example.Sum.*;
+import static org.example.Count.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import  org.mockito.Mockito;
+
 public class Tests {
     private int a = 2;
     private int b = 2;
     private int c = 5;
     private int d = 0;
+    private int method;
+    private int method_compare;
+    private int method_compare_zero;
+    private int method_diff;
+    private int method_test_diff_neg;
+    private int method_div;
 
+    @Before
+    public void SetUp(){
+        method = sum(a, b);
+        method_compare = compare(a,b);
+        method_compare_zero = compare(a, d);
+        method_diff = diff(a,b);
+        method_test_diff_neg = diff(a,c);
+        method_div = div(a,b);
+
+
+    }
     @After
+
+    @Test
     public void test_sum() {
-        int result = sum(a, b);
+        int result = method;
         assertEquals(4, result);
     }
     @Test
     public void test_compare() {
-        int result = compare(a, b);
+        int result = method_compare;
         assertEquals(4, result);
     }
     @Test
     public void test_compare_zero() {
-        int result_null = compare(a, d);
+        int result_null = method_compare_zero;
         assertEquals(0, result_null);
     }
 
     @Test
     public void test_diff() {
-        int result = diff(a, b);
+        int result = method_diff;
         assertEquals(result, 0);
     }
     @Test
     public void test_diff_neg() {
-        int result_negative = diff(a, c);
+        int result_negative = method_test_diff_neg;
         assertEquals(result_negative, -3);
     }
 
     @Test
     public void test_div() {
-        int result = div(a, b);
+        int result = method_div;
         assertEquals(result, 1);
     }
     @Test
     public void test_div_exception(){
-        assertThrows(RuntimeException.class, () -> Sum.div(a,d));
+        assertThrows(RuntimeException.class, () -> Count.div(a,d));
 
     }
 }
